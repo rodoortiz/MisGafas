2da000b3743196fc43e638ab2a04163906e41015
Rodolfo Ortiz <rodolfortiz94@gmail.com>
2020-02-10 11:36:49 -0600

    Proyect clean

---
+++ MisGafas/MisGafas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MisGafas
+//
+//  Created by Rodolfo Ortiz on 10/02/20.
+//  Copyright © 2020 Rodolfo Ortiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
