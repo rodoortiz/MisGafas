e441ca5ba37712b3a943d006ee46837a4871e22d
Rodolfo Ortiz <rodolfortiz94@gmail.com>
2020-02-23 16:29:32 -0600

    Checkout implemented

--- MisGafas/MisGafas/ViewControllers/CheckoutViewController.swift
+++ MisGafas/MisGafas/ViewControllers/CheckoutViewController.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2020 Rodolfo Ortiz. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class CheckoutViewController: UIViewController {
+   
+    @IBAction func didTapOrder(_ sender: Any) {
+        let alertController = UIAlertController(title: "Successful order", message: "Your order has been made", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default) { (UIAlertAction) -> Void in
+               self.dismiss(animated: true, completion: nil)
+        })
+
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboard()
+    }
+
+}
+
+extension UIViewController {
+    func hideKeyboard() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+}
